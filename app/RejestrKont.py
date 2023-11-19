9a42d854d7fca738371167e37e2a2b60be6da4ae
Alicja Chmura <ala.chmura2003@gmail.com>
2023-11-19 15:45:28 +0100

    Finished Lab6

--- app/RejestrKont.py
+++ app/RejestrKont.py
@@ -7,9 +7,9 @@ class Rejestr_Kont:
 
     @classmethod
     def find_account(cls, pesel):
-        account = [account for account in cls.accounts if account.pesel == pesel]
+        account = [account for account in cls.accounts if account.pesel == pesel][0]
         return account
 
     @classmethod
     def how_many_accounts(cls):
-        return len(cls.lista)
+        return len(cls.accounts)
--- app/tests/test_rejestr_kont.py
+++ app/tests/test_rejestr_kont.py
@@ -1,8 +1,7 @@
 import unittest
-
+from parameterized import parameterized
 from ..KontoOsobiste import Konto_Personal
 from ..RejestrKont import Rejestr_Kont
-# from parameterized import parameterized
 
 class TestRejestr(unittest.TestCase):
     name = "Dariusz"
@@ -11,24 +10,23 @@ class TestRejestr(unittest.TestCase):
 
     # Feature 14
     def setUp(self):
-         Rejestr_Kont.accounts = []
-
-    @classmethod
-    def setUpClass(cls):
-         konto = Konto_Personal(cls.name, cls.surname, cls.pesel)
-         Rejestr_Kont.add_account(konto)
-
-    def test_add_account(self):
-         konto = Konto_Personal(self.name, self.surname, self.pesel)
-         Rejestr_Kont.add_account(konto)
-         self.assertEqual()
-
-    def test_find_account(self):
-         self.assertEqual()
-    
-    def test_how_many_accounts(self):
-         self.assertEqual()
+         self.konto = Konto_Personal(self.name, self.surname, self.pesel)
 
     @classmethod
     def tearDownClass(cls):
          Rejestr_Kont.accounts = []
+         
+    @parameterized.expand([
+         ("name1", "surname1", "12345678901", 1),
+         ("name2", "surname2", "12345678902", 2),
+         ("name3", "surname3", "12345678903", 3)
+    ])
+    def test_add_account(self, name, surname, pesel, howManyAccountsNow):
+         konto = Konto_Personal(name, surname, pesel)
+         Rejestr_Kont.add_account(konto)
+         self.assertEqual(Rejestr_Kont.how_many_accounts(), howManyAccountsNow, "Number of accounts is not calculated correctly!")
+
+    def test_find_account(self):
+         Rejestr_Kont.add_account(self.konto)
+         foundAccount = Rejestr_Kont.find_account(self.pesel)
+         self.assertEqual(foundAccount, self.konto, "The account wasn't correctly found!")
